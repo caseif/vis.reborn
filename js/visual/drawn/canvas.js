2eefd066a168d31d013353f71df4861d4006e693
Max Roncace <me@caseif.net>
2017-07-01 14:53:50 -0400

    Disable canvas shake

--- js/visual/drawn/canvas.js
+++ js/visual/drawn/canvas.js
@@ -3,16 +3,6 @@ let Canvas = new function() {
     this.canvas;
     this.context;
 
-    const WAVE_DURATION = Math.PI / 8;
-    let waveFrameX = 0;
-    let waveFrameY = 0;
-    let waveSpeedX = 1;
-    let waveSpeedY = 1;
-    let waveAmplitudeX = 1;
-    let waveAmplitudeY = 1;
-    let trigX = Math.round(Math.random());
-    let trigY = Math.round(Math.random());
-
     let glow = Util.getCookie("glow") !== "false";
 
     let edge = window.navigator.userAgent.indexOf("Edge") > -1;
@@ -24,8 +14,6 @@ let Canvas = new function() {
         this.setStyling();
 
         Callbacks.addCallback(clearCallback, Priority.FIRST);
-        Callbacks.addCallback(shakeCallback, Priority.FIRST);
-        Callbacks.addCallback(postCallback, Priority.LAST);
     }
     
     this.setStyling = function() {
@@ -43,38 +31,6 @@ let Canvas = new function() {
 
     let clearCallback = function() {
         Canvas.context.clearRect(0, 0, Canvas.canvas.width, Canvas.canvas.height);
-    }
-
-    let shakeCallback = function(_, multiplier) {
-        Canvas.context.save();
-
-        let step = Config.maxShakeIntensity * multiplier;
-        waveFrameX += step * waveSpeedX;
-        if (waveFrameX > WAVE_DURATION) {
-            waveFrameX = 0;
-            waveAmplitudeX = Util.random(Config.minShakeScalar, Config.maxShakeScalar);
-            waveSpeedX = Util.random(Config.minShakeScalar, Config.maxShakeScalar) * (Math.random() < 0.5 ? -1 : 1);
-            trigX = Math.round(Math.random());
-        }
-        waveFrameY += step * waveSpeedY;
-        if (waveFrameY > WAVE_DURATION) {
-            waveFrameY = 0;
-            waveAmplitudeY = Util.random(Config.minShakeScalar, Config.maxShakeScalar);
-            waveSpeedY = Util.random(Config.minShakeScalar, Config.maxShakeScalar) * (Math.random() < 0.5 ? -1 : 1);
-            trigY = Math.round(Math.random());
-        }
-
-        let trigFuncX = trigX == 0 ? Math.cos : Math.sin;
-        let trigFuncY = trigY == 0 ? Math.cos : Math.sin;
-
-        let dx = trigFuncX(waveFrameX) * Config.maxShakeDisplacement * waveAmplitudeX * multiplier;
-        let dy = trigFuncY(waveFrameY) * Config.maxShakeDisplacement * waveAmplitudeY * multiplier;
-
-        Canvas.context.translate(dx, dy);
-    }
-
-    let postCallback = function() {
-        Canvas.context.restore();
     }
 
 }
